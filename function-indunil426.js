758f09eba89944d944ca6c0f2ffa3ec8c93f4304
Indunil1992 <indunil@adroitlogic.com>
2020-02-25 16:35:46 +0530

    add cmnt

---
+++ function-indunil426.js
@@ -0,0 +1,4 @@
+exports.handler = function(request, response) {
+    // cmnt
+    response.send({"message": "Successfully executed"});
+}
---
+++ index.js
@@ -0,0 +1,4 @@
+// This file is used to register all your cloud functions in GCP.
+// DO NOT EDIT/DELETE THIS, UNLESS YOU KNOW WHAT YOU ARE DOING.
+
+exports.functionIndunil426 = require("./function-indunil426.js").handler;
